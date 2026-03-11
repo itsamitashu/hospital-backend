66ba331d1e49e47c9fc771e6b68b87ab7ac6e1bd
Amit Kumar <myselfamitkumar30@gmail.com>
2026-03-11 20:22:45 +0530

    render port fix

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-const express = require("express")
-const mongoose = require("mongoose")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hospital Backend API Running");
+});
+
 app.use("/patients", require("./routes/patientRoutes"));
+
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected"))
-.catch(err=> console.log(err))
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
- console.log("Server running on port", PORT);
+  console.log("Server running on port", PORT);
 });
