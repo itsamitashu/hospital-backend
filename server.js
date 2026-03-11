876deeb61964be11bd0f7bcfcad54d1e9917ed49
Amit Kumar <myselfamitkumar30@gmail.com>
2026-03-11 20:13:37 +0530

    fix render port

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ require("dotenv").config()
 const app = express()
 
 app.use(express.json())
-
+app.use("/patients", require("./routes/patientRoutes"));
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB Connected"))
 .catch(err=> console.log(err))
 
-app.listen(5000,()=>{
- console.log("Server running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+ console.log("Server running on port", PORT);
+});
